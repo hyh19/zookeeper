c2ee1439f5a032eeb200877f11ea8ba1bc2f34ba
mrhuangyuhui <mrhuangyuhui@gmail.com>
2018-04-21 15:16:22 +0800

    * 重构代码

--- paxos-zookeeper-demos/src/main/java/chapter05/$5_3_1/ZooKeeperConstructorUsageSimple.java
+++ paxos-zookeeper-demos/src/main/java/chapter05/$5_3_1/ZooKeeperConstructorUsageSimple.java
@@ -1,6 +1,5 @@
 package chapter05.$5_3_1;
 
-import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
@@ -9,30 +8,31 @@ import java.util.concurrent.CountDownLatch;
 /**
  * 5.3.1 清单 5-2 创建一个最基本的 ZooKeeper 会话实例
  */
-public class ZooKeeperConstructorUsageSimple implements Watcher {
+public class ZooKeeperConstructorUsageSimple {
 
     private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
 
     public static void main(String[] args) throws Exception {
 
-        ZooKeeper zooKeeper = new ZooKeeper("domain1.book.zookeeper:2181", 5000,
-                new ZooKeeperConstructorUsageSimple());
+        ZooKeeper zooKeeper = new ZooKeeper("localhost:2181", 5000,
+                event -> {
+                    System.out.println("Receive watched event：" + event);
+                    // 收到会话连接成功通知
+                    if (Watcher.Event.KeeperState.SyncConnected == event.getState()) {
+                        // 解除阻塞
+                        connectedSemaphore.countDown();
+                    }
+
+                });
 
         System.out.println(zooKeeper.getState());
 
         try {
+            // 阻塞线程，等待会话连接成功。
             connectedSemaphore.await();
         } catch (InterruptedException e) {
         }
-        System.out.println("ZooKeeper session established.");
-    }
 
-    @Override
-    public void process(WatchedEvent event) {
-        System.out.println("Receive watched event：" + event);
-        // 收到会话连接成功通知
-        if (Event.KeeperState.SyncConnected == event.getState()) {
-            connectedSemaphore.countDown();
-        }
+        System.out.println("ZooKeeper session established.");
     }
 }
--- paxos-zookeeper-demos/src/main/java/chapter05/$5_3_1/ZooKeeperConstructorUsageWithSIDPASSWD.java
+++ paxos-zookeeper-demos/src/main/java/chapter05/$5_3_1/ZooKeeperConstructorUsageWithSIDPASSWD.java
@@ -1,6 +1,5 @@
 package chapter05.$5_3_1;
 
-import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
@@ -9,14 +8,22 @@ import java.util.concurrent.CountDownLatch;
 /**
  * 5.3.1 清单 5-3 复用 sessionId 和 sessionPasswd 来创建一个 ZooKeeper 对象实例
  */
-public class ZooKeeperConstructorUsageWithSIDPASSWD implements Watcher {
+public class ZooKeeperConstructorUsageWithSIDPASSWD {
 
     private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
 
     public static void main(String[] args) throws Exception {
 
-        ZooKeeper zooKeeper = new ZooKeeper("domain1.book.zookeeper:2181", 5000,
-                new ZooKeeperConstructorUsageWithSIDPASSWD());
+        String connectString = "localhost:2181";
+        Watcher watcher = event -> {
+            System.out.println("Receive watched event：" + event);
+            if (Watcher.Event.KeeperState.SyncConnected == event.getState()) {
+                connectedSemaphore.countDown();
+            }
+        };
+
+        // 第一次连接会话
+        ZooKeeper zooKeeper = new ZooKeeper(connectString, 5000, watcher);
 
         connectedSemaphore.await();
 
@@ -24,20 +31,13 @@ public class ZooKeeperConstructorUsageWithSIDPASSWD implements Watcher {
         byte[] passwd = zooKeeper.getSessionPasswd();
 
         // 使用错误的会话 ID 和 密码，服务器将返回连接超时事件。
-        zooKeeper = new ZooKeeper("domain1.book.zookeeper:2181", 5000,
-                new ZooKeeperConstructorUsageWithSIDPASSWD(), 1l, "wrong_passwd".getBytes());
+        zooKeeper = new ZooKeeper(connectString, 5000,
+                watcher, 1L, "apple".getBytes());
 
         // 使用正确的会话 ID 和 密码，服务器将返回连接成功事件。
-        zooKeeper = new ZooKeeper("domain1.book.zookeeper:2181", 5000,
-                new ZooKeeperConstructorUsageWithSIDPASSWD(), sessionId, passwd);
+        zooKeeper = new ZooKeeper(connectString, 5000,
+                watcher, sessionId, passwd);
 
         Thread.sleep(Integer.MAX_VALUE);
-    }
-
-    public void process(WatchedEvent event) {
-        System.out.println("Receive watched event：" + event);
-        if (Event.KeeperState.SyncConnected == event.getState()) {
-            connectedSemaphore.countDown();
-        }
     }
 }
