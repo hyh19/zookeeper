66c97e2d3a699fd69d105a70f6b08976afdbcb43
mrhuangyuhui <mrhuangyuhui@gmail.com>
2018-04-21 16:50:16 +0800

    * 重构代码

--- paxos-zookeeper-demos/src/main/java/chapter05/$5_3_3/DeleteAPIAsyncUsage.java
+++ paxos-zookeeper-demos/src/main/java/chapter05/$5_3_3/DeleteAPIAsyncUsage.java
@@ -1,6 +1,5 @@
 package chapter05.$5_3_3;
 
-import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
@@ -9,33 +8,27 @@ import java.util.concurrent.CountDownLatch;
 /**
  * 5.3.3 使用异步方式删除节点
  */
-public class DeleteAPIAsyncUsage implements Watcher {
+public class DeleteAPIAsyncUsage {
 
     private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
 
     public static void main(String[] args) throws Exception {
 
-        ZooKeeper zookeeper = new ZooKeeper("domain1.book.zookeeper:2181",
-                5000, new DeleteAPIAsyncUsage());
+        ZooKeeper zookeeper = new ZooKeeper("localhost:2181",
+                5000, event -> {
+            if (Watcher.Event.KeeperState.SyncConnected == event.getState()) {
+                connectedSemaphore.countDown();
+            }
+        });
 
-        try {
-            connectedSemaphore.await();
-        } catch (InterruptedException e) {
-        }
-        System.out.println("ZooKeeper session established.");
+        connectedSemaphore.await();
 
-        // 在服务器上直接用命令行创建节点 create /zk-test Hello
+        // 创建测试节点 create -e /fruit apple
         // -1 表示匹配任何版本
-        zookeeper.delete("/zk-test", -1, (rc, path, ctx) -> System.out.println("Success delete znode: " + path), "Hello ZooKeeper");
+        zookeeper.delete("/fruit", -1,
+                (rc, path, ctx) -> System.out.println("删除节点 " + path),
+                null);
 
         Thread.sleep(Integer.MAX_VALUE);
-    }
-
-    @Override
-    public void process(WatchedEvent event) {
-        System.out.println("Receive watched event：" + event);
-        if (Event.KeeperState.SyncConnected == event.getState()) {
-            connectedSemaphore.countDown();
-        }
     }
 }
--- paxos-zookeeper-demos/src/main/java/chapter05/$5_3_3/DeleteAPISyncUsage.java
+++ paxos-zookeeper-demos/src/main/java/chapter05/$5_3_3/DeleteAPISyncUsage.java
@@ -1,37 +1,32 @@
 package chapter05.$5_3_3;
 
-import org.apache.zookeeper.*;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 import java.util.concurrent.CountDownLatch;
 
 /**
  * 5.3.3 使用同步方式删除节点
  */
-public class DeleteAPISyncUsage implements Watcher {
+public class DeleteAPISyncUsage {
 
     private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
 
     public static void main(String[] args) throws Exception {
 
-        ZooKeeper zookeeper = new ZooKeeper("domain1.book.zookeeper:2181",
-                5000, new DeleteAPISyncUsage());
+        ZooKeeper zookeeper = new ZooKeeper("localhost:2181",
+                5000, event -> {
+            if (Watcher.Event.KeeperState.SyncConnected == event.getState()) {
+                connectedSemaphore.countDown();
+            }
+        });
 
-        try {
-            connectedSemaphore.await();
-        } catch (InterruptedException e) {
-        }
-        System.out.println("ZooKeeper session established.");
+        connectedSemaphore.await();
 
-        // 在服务器上直接用命令行创建节点 create /zk-test Hello
-        String path = "/zk-test";
+        // 创建测试节点 create -e /fruit apple
         // -1 表示匹配任何版本
-        zookeeper.delete(path, -1);
-        System.out.println("Success delete znode: " + path);
-    }
+        zookeeper.delete("/fruit", -1);
 
-    public void process(WatchedEvent event) {
-        if (Event.KeeperState.SyncConnected == event.getState()) {
-            connectedSemaphore.countDown();
-        }
+        Thread.sleep(Integer.MAX_VALUE);
     }
 }
