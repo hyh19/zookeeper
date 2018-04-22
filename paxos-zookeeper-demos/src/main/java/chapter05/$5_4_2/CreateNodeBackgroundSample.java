2cd9c63dc78321f6ddecfa1c11eafc5562adcc97
mrhuangyuhui <mrhuangyuhui@gmail.com>
2018-04-22 17:30:04 +0800

    * 重构

--- paxos-zookeeper-demos/src/main/java/chapter05/$5_4_2/CreateNodeBackgroundSample.java
+++ paxos-zookeeper-demos/src/main/java/chapter05/$5_4_2/CreateNodeBackgroundSample.java
@@ -3,25 +3,26 @@ package chapter05.$5_4_2;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
-import org.apache.zookeeper.CreateMode;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import static org.apache.zookeeper.CreateMode.PERSISTENT;
 
 /**
  * 5.4.2 清单 5-33 Curator 异步 API 使用示例
  */
 public class CreateNodeBackgroundSample {
 
-    static CuratorFramework client = CuratorFrameworkFactory.builder()
-            .connectString("206.189.89.184:2181")
+    private static final CuratorFramework client = CuratorFrameworkFactory.builder()
+            .connectString("localhost:2181")
             .sessionTimeoutMs(5000)
             .retryPolicy(new ExponentialBackoffRetry(1000, 3))
             .build();
 
-    static CountDownLatch semaphore = new CountDownLatch(2);
-    static ExecutorService threadPool = Executors.newFixedThreadPool(2);
+    private static final CountDownLatch semaphore = new CountDownLatch(2);
+    private static final ExecutorService threadPool = Executors.newFixedThreadPool(2);
 
     public static void main(String[] args) throws Exception {
 
@@ -30,22 +31,25 @@ public class CreateNodeBackgroundSample {
 
         // 传入自定义的 Executor
         String apple = "/apple";
-        client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT)
+        client.create().creatingParentsIfNeeded().withMode(PERSISTENT)
                 .inBackground((client, event) -> {
-                    System.out.println("path[" + apple + "] event[code: " + event.getResultCode() + ", type: " + event.getType() + "]");
+                    System.out.println("path: " + apple + " event[code: " + event.getResultCode() + ", type: " + event.getType() + "]");
                     System.out.println("path: " + apple + " Thread of processResult: " + Thread.currentThread().getName());
                     semaphore.countDown();
                 }, threadPool).forPath(apple, "apple".getBytes());
 
+
         // 没有传入自定义的 Executor
         String banana = "/banana";
-        client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT)
+        client.create().creatingParentsIfNeeded().withMode(PERSISTENT)
                 .inBackground((client, event) -> {
-                    System.out.println("path[" + banana + "] event[code: " + event.getResultCode() + ", type: " + event.getType() + "]");
+                    System.out.println("path: " + banana + " event[code: " + event.getResultCode() + ", type: " + event.getType() + "]");
                     System.out.println("path: " + banana + " Thread of processResult: " + Thread.currentThread().getName());
                     semaphore.countDown();
                 }).forPath("/banana", "banana".getBytes());
+
         semaphore.await();
+
         threadPool.shutdown();
     }
 }
--- paxos-zookeeper-demos/src/main/java/chapter05/$5_4_2/NodeCacheSample.java
+++ paxos-zookeeper-demos/src/main/java/chapter05/$5_4_2/NodeCacheSample.java
@@ -10,28 +10,31 @@ import org.apache.curator.retry.ExponentialBackoffRetry;
  */
 public class NodeCacheSample {
 
-    static String path = "/pet";
-    static CuratorFramework client = CuratorFrameworkFactory.builder()
-            .connectString("domain1.book.zookeeper:2181")
+    private static final CuratorFramework client = CuratorFrameworkFactory.builder()
+            .connectString("localhost:2181")
             .sessionTimeoutMs(5000)
             .retryPolicy(new ExponentialBackoffRetry(1000, 3))
             .build();
 
     public static void main(String[] args) throws Exception {
-        /**
-         * 创建节点 create /pet dog
-         * 更新数据 set /pet cat
-         * 更新数据 set /pet pig
-         * 删除节点 delete /pet
-         */
+
         client.start();
-        final NodeCache cache = new NodeCache(client, path, false);
+
+        /*
+          程序运行过程中
+          创建节点 create /pet dog
+          更新数据 set /pet cat
+          更新数据 set /pet pig
+          删除节点 delete /pet
+          注意观察事件通知
+         */
+        final NodeCache cache = new NodeCache(client, "/pet", false);
         // true 表示第一次启动时立即读取节点的数据，并保存在 Cache 中。
         cache.start(true);
         // 注册监听器
         cache.getListenable().addListener(() -> {
             // 数据变更通知
-            System.out.println("Node data update, new data: " +
+            System.out.println("new data: " +
                     new String(cache.getCurrentData().getData()));
         });
 
